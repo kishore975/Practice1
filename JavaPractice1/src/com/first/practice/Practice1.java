07b217f5f69f1c35400cad5e39992e7ec4bad1df
user <user@DESKTOP-CPP9EL0>
2024-04-27 17:30:22 +0530

    Updated the application

--- JavaPractice1/src/com/first/practice/Practice1.java
+++ JavaPractice1/src/com/first/practice/Practice1.java
@@ -43,31 +43,82 @@ public class Practice1
 
 				//Return age eligibility ends
 					
-				public static void main(String args[]) {
+				public static void main(String args[]) 
+				{
 					Practice1 obj=new Practice1();
 					obj.run();
+					
 				}
-				public void run() {
+				
+				public void run() 
+			{
 				Scanner scan=new Scanner(System.in);
 				System.out.println("Enter your Company : ");
-				System.out.println("Enter your age : ");
 				String cName=scan.nextLine();
+				System.out.println("Enter your age : ");
+				
 				int 	aAge=scan.nextInt();
-				if(cName.equalsIgnoreCase("TCS") && aAge<=21) {
-				System.out.println("You are eligible to 10K run");	
+if(cName.equalsIgnoreCase("TCS") && aAge<60) {
+				if(cName.equalsIgnoreCase("TCS") && aAge<=21) 
+				{
+				System.out.println("You are eligible to 10K run");
+				System.out.println("NOW");
 				}
-				else if(cName.equalsIgnoreCase("TCS") && (aAge>=21&&aAge<41)){
-					System.out.println("You are eligible to 5K run");	
+				else if(cName.equalsIgnoreCase("TCS") && (aAge>=21&&aAge<41))
+				{
+					System.out.println("You are eligible to 5K run");
+					System.out.println("NOW");
 				}
+				
 				else if(cName.equalsIgnoreCase("TCS") && aAge>=41&&aAge<50) {
 					System.out.println("You are eligible to 3K run");
+					System.out.println("NOW");
+				}
+				Scanner scan1=new Scanner(System.in);
+				System.out.println("Enter your State : ");
+				String state=scan1.nextLine();
+				
+				if(state.equalsIgnoreCase("Telangana"))
+				{
+					Scanner scan2=new Scanner(System.in);
+					System.out.println("Enter your City : ");
+					String city=scan2.nextLine();
+					if(city.equalsIgnoreCase("Gachibowli")) 
+					{
+						System.out.println("You should visit to Participate in Synergy Park");	
+					}
+					else if(city.equalsIgnoreCase("Hitechcity"))
+					{
+						System.out.println("You should visit to Participate in Kohinoor Park");	
+					}
+						
+					else if(city.equalsIgnoreCase("Raidurg")) 
+					{
+						System.out.println("You should visit to Participate in Deccan Park");
+					}
+					else if(city.equalsIgnoreCase("Adibatla")) 
+					{
+						System.out.println("You should visit to  Participate in Adibatla Office");
+					}
+					else {
+						System.out.println("No such location in Telangana");
+					}
 				}
 				else {
-					System.out.println("You might not  from TCS or not have Right Age or something else");
-				}
+					System.out.println("You are not from Telangana");
 				}
 				
 					
-
-
 }
+else {
+	System.out.println("Not match to Company or Age");
+}
+				
+				
+				
+	}
+							
+}			
+
+
+
